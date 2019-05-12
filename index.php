114842d51dac54874ce81532e4dd5489ada9bcfc
Antonio Díaz <j_antonio_ud@hotmail.com>
2019-05-12 18:43:52 -0500

    asdas

--- index.php
+++ index.php
@@ -24,7 +24,47 @@
 </head>
 
 <body>
-      <?php require_once('inc/header.php')?>
+<header class="site-header">
+        <div class="hero">
+          <div class="contenido-header">
+            <nav class="redes-sociales">
+              <a href="#"><i class="fab fa-facebook-f"></i></a>
+              <a href="#"><i class="fab fa-twitter"></i></a>
+              <a href="#"><i class="fab fa-instagram"></i></a> 
+        
+            </nav>
+            <div class="informacion-evento">
+              <div class="clearfix">
+                  <p class="fecha"></i>Alayon puto</p>
+                  <p class="ciudad"><i class="fa fa-map-marker" aria-hidden="true"></i>Veracruz, MX</p>
+              </div>
+            <h1 class="nombre-sitio"><span>Movie</span>Reviewer</h1>
+            <p class="slogan">Si nosotros no la toleramos menos <span> tu podras </span></p>
+            </div> <!-- .informacion-evento -->
+            
+          </div>
+        </div><!-- .hero -->
+
+      </header>
+      <!-- Barra de navegación -->
+      <div class="barra">
+        <div class="contenedor clearfix">
+          <div class="logo clearfix">
+            <img src="img/logodif.svg" alt="logo moviereviewer">
+          </div>
+          <div class="menu-movil clearfix">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <nav class="navegacion-principal clearfix">
+            <a href="#">INICIO</a>
+            <a href="#">PELICULAS</a>
+            <a href="#">INICIAR SESIÓN</a>
+            <a href="#">REGISTRARSE</a>
+          </nav> <!-- Navegador Principal-->
+        </div><!-- Contenedor -->
+      </div><!-- Barra de navegación -->
 
       <section class="seccion contenedor">
         <h2>La mejor pagina de reviewers sobre peliculas</h2>
@@ -34,8 +74,7 @@
             dieron los usuarios.
         </p>
       </section> <!--.Sección -->
-
-      <section class="spec-moviereviewer  ">
+      <section class="spec-moviereviewer">
         <div class="contenedor-video clearfix">
             <video autoplay loop poster="posterimage.jpg">
                 <source src="video/La sala de cine.mp4"   type="video/mp4">
